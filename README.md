73fe6878cfb4b9f46dec5b1711c0ac5c68abe5b4
tinbuzancic <tinbuzancic@gmail.com>
2018-10-04 20:27:13 -0400

    added server stuff

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # vue-project setup
+
+# video reference
 https://www.youtube.com/watch?v=Fa4cRMaTDUI&t=925s
+
+# clone github repo
+https://github.com/codyseibert/tab-tracker.git
+
+# install Vue.js boilerplate to initiate a Vue.js front-end application
 npm install -g vue-cli
-vue init webpack client
+
+# initialize the webpack and npm dependencies
 # run vue
 vue init webpack client
--- server/src/app.js
+++ server/src/app.js
@@ -1,28 +1,18 @@
-console.log("Listening...")
-
-//init modules
+// imports
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
-//build up the server
-const app = express()
-//Format logs in a certain manner
+var app = express()
 app.use(morgan('combined'))
-//Allow our app to easily parse JSON data passed in
 app.use(bodyParser.json())
-//Server to be hosted on a different domain and can be hit from anywhere
 app.use(cors())
 
-/*
-* get, post, put, patch, delete - main five endpoint methods
-*/
 app.get('/status', (req, res) => {
-    res.send({
-        messaage: 'Status Received.'
-    })
+  res.send({
+    message: 'Hello World!'
+  })
 })
 
-//Listen for Server running on port:8081
 app.listen(process.env.PORT || 8081)
